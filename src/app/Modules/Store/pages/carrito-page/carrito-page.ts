305c46bff2ca1f947389a19d5b4e6221d4c90e46
C3s4r310 <cu046481@gmail.com>
2026-04-28 16:57:02 +0000

    Feat: Añadi el carrito page css -html

--- src/app/Modules/Store/pages/carrito-page/carrito-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-carrito-page',
-  imports: [],
-  templateUrl: './carrito-page.html',
-  styleUrl: './carrito-page.css',
-})
-export class CarritoPage {}
