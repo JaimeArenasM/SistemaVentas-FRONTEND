69558bbd92df7d37c34f20ffdff26c8a17e2dcab
ArbiManuel <u23225486@utp.edu.pe>
2026-04-29 15:50:50 -0500

    shared creado para productos

---
+++ src/app/Shared/Components/product-confirm-dialog/product-confirm-dialog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-confirm-dialog',
+  imports: [],
+  templateUrl: './product-confirm-dialog.html',
+  styleUrl: './product-confirm-dialog.css',
+})
+export class ProductConfirmDialog {}
