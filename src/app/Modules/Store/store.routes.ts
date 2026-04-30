afc1df4be79b5639611b9446014896a14a79d96e
IsaacLCH12 <isaaclivaque123@gmail.com>
2026-04-30 08:23:40 -0500

    feat(store): agregar vista de mi perfil para  que el cliente pueda modificar sus datos

--- src/app/Modules/Store/store.routes.ts
+++ src/app/Modules/Store/store.routes.ts
@@ -7,6 +7,7 @@ import { TermsComponent } from "./pages/terminos-page/terminos-page";
 import { MisComprasPage } from "./pages/mis-compras-page/mis-compras-page";
 import { PoliticaprivPage } from "./pages/politicapriv-page/politicapriv-page";
 import { authGuard } from "../../Core/Guards/auth-guard";
+import { MiPerfilPage } from "./pages/mi-perfil-page/mi-perfil-page";
 
 
 
@@ -35,6 +36,10 @@ path: 'carrito',component:CarritoPage
 {
   path:'politica-de-privacidad',
   component: PoliticaprivPage
+},
+{
+  path:'perfil',
+  component: MiPerfilPage, canActivate:[authGuard]
 },
 {
   path: 'checkout', component: CheckoutPage, canActivate: [authGuard]
