7dfad6c6f538dc5d0e90e11653f6adb5dd9e2974
Casey1304 <casey.r.s1304@gmail.com>
2026-05-29 23:31:33 -0500

    feat: rediseño completo del panel administrativo

--- src/app/Shared/Components/user-form-dialog/user-form-dialog.ts
+++ src/app/Shared/Components/user-form-dialog/user-form-dialog.ts
@@ -1,12 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef
+} from '@angular/material/dialog';
+
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogRef, MatDialogContent, MatDialogModule } from '@angular/material/dialog';
-import { MatIcon, MatIconModule } from "@angular/material/icon";
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-user-form-dialog',
+  standalone: true,
   imports: [
     CommonModule,
     MatDialogModule,
@@ -17,48 +29,47 @@ import { MatIcon, MatIconModule } from "@angular/material/icon";
   templateUrl: './user-form-dialog.html',
   styleUrl: './user-form-dialog.css',
 })
-export class UserFormDialog {
+export class UserFormDialog implements OnInit {
 
+  userForm: FormGroup;
+  esEdicion: boolean = false;
 
-    userForm: FormGroup;
-    esEdicion: boolean= false;
+  constructor(
+    private fb: FormBuilder,
+    public dialogRef: MatDialogRef<UserFormDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.dialogRef.updateSize('560px', 'auto');
 
-    constructor(
-      private fb: FormBuilder,
-      public dialogRef: MatDialogRef<UserFormDialog>,
+    this.userForm = this.fb.group({
+      iIdUsuario: [new Date().getTime()],
+      vUsuario: ['', [Validators.required, Validators.email]],
+      password: ['123456'],
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      dni: ['', [Validators.required, Validators.minLength(8)]],
+      telefono: ['', [Validators.required, Validators.minLength(9)]],
+      iIdTipoUsuario: [2, Validators.required]
+    });
+  }
 
-      @Inject(MAT_DIALOG_DATA) public data: any
-    ){
-      this.userForm=this.fb.group({
-
-        iIdUsuario: [new Date().getTime()],
-        vUsuario:['',[Validators.required, Validators.email]],
-        password: ['123456'],
-        nombres: ['',Validators.required],
-        apellidos:['',Validators.required],
-        dni:['',[Validators.required,Validators.minLength(8)]],
-        telefono:['',[Validators.required,Validators.minLength(9)]],
-        iIdTipoUsuario:[2,Validators.required]
-      });
-    }
-
-    ngOnInit(): void {
-      if (this.data) {
-        this.esEdicion=true;
-
-        this.userForm.patchValue(this.data);
-      }
-    }
-
-    onCancel(): void{
-      this.dialogRef.close(null);
-    }
-
-    onSave():void{
-      if(this.userForm.valid){
-        this.dialogRef.close(this.userForm.value);
-      }else{
-        this.userForm.markAllAsTouched() /**muestra errores si faltan campos */;
-      }
+  ngOnInit(): void {
+    if (this.data) {
+      this.esEdicion = true;
+      this.userForm.patchValue(this.data);
     }
   }
+
+  onCancel(): void {
+    this.dialogRef.close(null);
+  }
+
+  onSave(): void {
+    if (this.userForm.valid) {
+      this.dialogRef.close(this.userForm.value);
+      return;
+    }
+
+    this.userForm.markAllAsTouched();
+  }
+}
