f35d1b52b5c9c7cd193e794fde073083d6e3e967
Casey1304 <casey.r.s1304@gmail.com>
2026-06-29 18:39:42 -0500

    ajustes cloudinary y dialog

--- src/app/Shared/Components/product-form-dialog/product-form-dialog.ts
+++ src/app/Shared/Components/product-form-dialog/product-form-dialog.ts
@@ -6,6 +6,7 @@ import { Product } from '../../../Core/Interfaces/IProduct.interface';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { CloudinaryService } from '../../../Core/Services/cloudinary.service';
 
 
 @Component({
@@ -26,11 +27,62 @@ export class ProductFormDialog {
   product: Product;
 
   constructor(
-    private dialogRef: MatDialogRef<ProductFormDialog>,
-    @Inject(MAT_DIALOG_DATA) public data: Product | null
-  ) {
-    this.product = data ? { ...data } : { id: 0, name: '', price: 0, image: '', description: '', category: '' };
-  }
+  private dialogRef: MatDialogRef<ProductFormDialog>,
+  @Inject(MAT_DIALOG_DATA) public data: Product | null,
+  private cloudinaryService: CloudinaryService
+) {
+  this.product = data
+    ? { ...data }
+    : {
+        id: 0,
+        name: '',
+        price: 0,
+        image: '',
+        description: '',
+        category: ''
+      };
+}
+selectedFile: File | null = null;
+previewUrl: string | null = null;
+uploading = false;
+
+onFileSelected(event: any) {
+  const file = event.target.files[0];
+
+  if (!file) return;
+
+  this.selectedFile = file;
+  this.previewUrl = URL.createObjectURL(file);
+}
+
+uploadImage() {
+ 
+  if (!this.selectedFile) return;
+
+  this.uploading = true;
+
+  this.cloudinaryService.uploadImage(this.selectedFile).subscribe({
+
+    next: (response) => {
+
+      this.product.image = response.secure_url;
+      
+      this.uploading = false;
+
+      alert('Imagen subida correctamente');
+    },
+
+    error: () => {
+
+      this.uploading = false;
+
+      alert('Error al subir la imagen');
+
+    }
+
+  });
+
+}
 
   onSave() { this.dialogRef.close(this.product); }
   onCancel() { this.dialogRef.close(null); }
